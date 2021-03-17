b72d56e21189446cd1a8ed1a18a8a273cdee5f59
wendy <wendy.wizardary@gmail.com>
2021-03-17 23:52:42 +0700

    d

--- src/frontend/containers/Public/RegisterContainer/PanelRegister/PanelCodeChecking.js
+++ src/frontend/containers/Public/RegisterContainer/PanelRegister/PanelCodeChecking.js
@@ -5,15 +5,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { checkingStudent } from "shared/datasources/user"
 
 function PanelCodeChecking({ onCallback }) {
-  const [studentCode, setStudentCode] = useState(null);
-  const [cardNo, setCardNo] = useState(null);
+  const [studentCode, setStudentCode] = useState(6310130019);
+  const [cardNo, setCardNo] = useState(1749900201835);
+  const [respMessage, setRespMessage] = useState(null);
 
-  const _handleSubmit = async(e) => {
+  const _handleSubmit = async (e) => {
     e.preventDefault()
 
     const result = await checkingStudent(studentCode, cardNo);
+
     if (result.success) {
       onCallback(true)
+    } else {
+      setRespMessage(result.message)
     }
   }
 
@@ -23,18 +27,22 @@ function PanelCodeChecking({ onCallback }) {
         <div className="input-inner">
           <FormGroup>
             <Label>ตรวจสอบรหัสนักศึกษา</Label>
-            <Input placeholder="ระบุรหัสนักศึกษา" onChange={(e) => setStudentCode(e.target.value)} />
+            <Input placeholder="ระบุรหัสนักศึกษา" value={studentCode} onChange={(e) => setStudentCode(e.target.value)} />
           </FormGroup>
           <FormGroup>
             <Label>รหัสประจำตัวประชาชน</Label>
-            <Input placeholder="ระบุตัวเลข 13 หลัก Ex. xxxxxxxxxxxxx" onChange={(e) => setCardNo(e.target.value)} />
+            <Input placeholder="ระบุตัวเลข 13 หลัก Ex. xxxxxxxxxxxxx" value={cardNo} onChange={(e) => setCardNo(e.target.value)} />
           </FormGroup>
         </div>
         <Button block color="primary">ตรวจสอบ</Button>
-        <Alert color="info">
-          <b><FontAwesomeIcon icon={faExclamationTriangle} /> หมายเหตุ</b>
-          <p>สมัครใช้งานได้เฉพาะนักศึกษาคณะวิศวกรรมศาสตร์ มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่เท่านั้น</p>
-        </Alert>
+        {
+          respMessage && (
+            <Alert color="danger">
+              <b><FontAwesomeIcon icon={faExclamationTriangle} /> Error</b>
+              <p>{respMessage}</p>
+            </Alert>
+          )
+        }
       </Form>
     </div>
   )
--- src/frontend/containers/Public/RegisterContainer/PanelRegister/PanelInputInfo.js
+++ src/frontend/containers/Public/RegisterContainer/PanelRegister/PanelInputInfo.js
@@ -1,35 +1,69 @@
-import React from "react"
+import React, { useRef } from "react"
+import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons"
-import { Form, FormGroup, Input, Label, Button } from "reactstrap"
+import { FormGroup, Label, Button } from "reactstrap"
 import "./index.css"
 
 function PanelInputInfo({ onCallback }) {
-  const _handleSubmit = (e) => {
-    e.preventDefault()
+  const { register, handleSubmit, watch, errors } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
 
-    onCallback(true)
+  const _handleSubmit = (value) => {
+    console.log(value)
+    //onCallback(true)
   }
   
   return (
     <div className="panel-input-info">
       <div className="block-register-email">
         <h3>สมัครด้วยอีเมล</h3>
-        <Form onSubmit={_handleSubmit}>
+        <form onSubmit={handleSubmit(_handleSubmit)}>
           <FormGroup>
             <Label for="email">อีเมล</Label>
-            <Input type="email" name="email" placeholder="example@mail.com" />
+            <input 
+              type="email" 
+              id="email" 
+              className="form-control"
+              ref={register("regist_email", {
+                required: true
+              })}
+              defaultValue="test@gmail.com"
+              placeholder="example@mail.com" 
+            />
+            {errors.regist_email?.type === "required" && <p className="validate-message">Field is required</p>}
           </FormGroup>
           <FormGroup>
             <Label for="password">รหัสผ่าน</Label>
-            <Input type="password" name="password" placeholder="ระบุด้วยอักขระ (a-z, A-Z, 0-9)" />
+            <input 
+              type="password" 
+              id="password" 
+              className="form-control"
+              ref={register("regist_password", {
+                required: true,
+                min: 8
+              })}
+              placeholder="ระบุด้วยอักขระ (a-z, A-Z, 0-9)" 
+            />
+            {errors.regist_password?.type === "required" && <p className="validate-message">Field is required</p>}
+            {errors.regist_password?.type === "min" && <p className="validate-message">Password at least 8 strings</p>}
           </FormGroup>
           <FormGroup>
             <Label for="cpassword">ยืนยันรหัสผ่าน</Label>
-            <Input type="password" name="cpassword" placeholder="ระบุรหัสผ่านอีกครั้ง" />
+            <input 
+              type="password" 
+              id="cpassword"
+              className="form-control"
+              ref={register("regist_cpassword", {
+                required: true,
+              })}              
+              placeholder="ระบุรหัสผ่านอีกครั้ง" 
+            />
+            {errors.cpassword && <p className="validate-message">{errors.cpassword.message}</p>}
           </FormGroup>
           <Button color="primary" block>สมัครเข้าใช้งาน</Button>
-        </Form>
+        </form>
       </div>
       <hr className="line" />
       <div className="block-register-social">
